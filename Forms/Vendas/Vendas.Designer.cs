ea2778ec377aa5a72aca2c948a02b92b90530592
AsheleyNascimento <asheleysk8@gmail.com>
2023-03-21 10:14:43 -0300

    feat: Forms  de vendas criado

---
+++ Forms/Vendas/Vendas.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MovSoft.Forms.Vendas
+{
+    partial class Vendas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewVendas = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.inputPesquisar = new System.Windows.Forms.TextBox();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnNovoPedido = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVendas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewVendas
+            // 
+            this.dataGridViewVendas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewVendas.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridViewVendas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewVendas.Location = new System.Drawing.Point(194, 100);
+            this.dataGridViewVendas.Name = "dataGridViewVendas";
+            this.dataGridViewVendas.RowTemplate.Height = 25;
+            this.dataGridViewVendas.Size = new System.Drawing.Size(300, 300);
+            this.dataGridViewVendas.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(505, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 300);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.Transparent;
+            this.btnPesquisar.BackgroundImage = global::MovSoft.Properties.Resources.icon_search;
+            this.btnPesquisar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnPesquisar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPesquisar.FlatAppearance.BorderSize = 0;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.btnPesquisar.Location = new System.Drawing.Point(440, 71);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(23, 23);
+            this.btnPesquisar.TabIndex = 6;
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            // 
+            // inputPesquisar
+            // 
+            this.inputPesquisar.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.inputPesquisar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.inputPesquisar.Location = new System.Drawing.Point(194, 71);
+            this.inputPesquisar.Name = "inputPesquisar";
+            this.inputPesquisar.PlaceholderText = "Pesquisar";
+            this.inputPesquisar.Size = new System.Drawing.Size(240, 23);
+            this.inputPesquisar.TabIndex = 5;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnEditar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEditar.Enabled = false;
+            this.btnEditar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnEditar.FlatAppearance.BorderSize = 0;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnEditar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnEditar.Location = new System.Drawing.Point(79, 158);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(100, 50);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnImprimir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnImprimir.Enabled = false;
+            this.btnImprimir.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnImprimir.FlatAppearance.BorderSize = 0;
+            this.btnImprimir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnImprimir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnImprimir.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnImprimir.Location = new System.Drawing.Point(79, 217);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(100, 50);
+            this.btnImprimir.TabIndex = 9;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = false;
+            // 
+            // btnNovoPedido
+            // 
+            this.btnNovoPedido.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnNovoPedido.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNovoPedido.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(133)))), ((int)(((byte)(132)))));
+            this.btnNovoPedido.FlatAppearance.BorderSize = 0;
+            this.btnNovoPedido.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovoPedido.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnNovoPedido.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnNovoPedido.Location = new System.Drawing.Point(79, 100);
+            this.btnNovoPedido.Name = "btnNovoPedido";
+            this.btnNovoPedido.Size = new System.Drawing.Size(100, 50);
+            this.btnNovoPedido.TabIndex = 7;
+            this.btnNovoPedido.Text = "Novo Pedido";
+            this.btnNovoPedido.UseVisualStyleBackColor = false;
+            // 
+            // Vendas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(931, 503);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnNovoPedido);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.inputPesquisar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridViewVendas);
+            this.Name = "Vendas";
+            this.Text = "MovSoft - Vendas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewVendas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private DataGridView dataGridViewVendas;
+        private Panel panel1;
+        private Button btnPesquisar;
+        private TextBox inputPesquisar;
+        private Button btnEditar;
+        private Button btnImprimir;
+        private Button btnNovoPedido;
+    }
+}
